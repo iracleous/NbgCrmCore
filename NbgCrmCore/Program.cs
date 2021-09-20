c661d8680f8409bae93e663a2c1b8acd4c2009ed
dimitris <iracleous@gmail.com>
2021-09-20 11:15:01 +0300

    testing Repositories

--- NbgCrmCore/Program.cs
+++ NbgCrmCore/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NbgCrmCore.Model;
+using NbgCrmCore.Repository;
+using System;
 
 namespace NbgCrmCore
 {
@@ -6,7 +8,35 @@ namespace NbgCrmCore
     {
         static void Main(string[] args)
         {
-            
+            Test2();
+        }
+
+
+        static void Test2()
+        {
+            using var db = new CrmDbContext();
+            UserRepository userRepository = new UserRepository(db);
+            var users = userRepository.RetreiveEntities(0, 0);
+            foreach(var user in users)
+            {
+                Console.WriteLine($" userId = {user.UserId}  email= {user.Email}");
+            }
+        }
+        static void Test1()
+        {
+            User user = new User()
+            {
+                Address = "Lamia",
+                Email = "a@a.gr",
+                FirstName = "Dimos",
+                LastName = "Nikolaou",
+                Password = "123456",
+                Username = "Dimos"
+            };
+            using var db = new CrmDbContext() ;
+            UserRepository userRepository = new UserRepository( db);
+            var response = userRepository.CreateEntity(user);
+            Console.WriteLine($"userId = {response.ReturnData.UserId}");
         }
     }
 }
---
+++ NbgCrmCore/Repository/UserRepository.cs
@@ -0,0 +1,67 @@
+﻿using NbgCrmCore.Model;
+using NbgCrmCore.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NbgCrmCore.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly CrmDbContext db;
+
+        public UserRepository(CrmDbContext _db)
+        {
+            db = _db;
+        }
+
+        public DbResponse<User> CreateEntity(User t)
+        {
+            db.Users.Add(t);
+            db.SaveChanges();
+            return new DbResponse<User> { 
+                ReturnCode = 0, 
+                ReturnData = t, 
+                ReturnDescription = "OK"
+            };
+        }
+
+        public ICollection<Basket> GetBaskets(int userId, int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HardDeleteEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> RetreiveEntities(int pageNumber, int pageSize)
+        {
+            return db.Users.ToList();
+        }
+
+        public DbResponse<User> RetreiveEntity(int id)
+        {
+            User user = db.Users.Find(id);
+            return new DbResponse<User>
+            {
+                ReturnCode = 0,
+                ReturnData = user,
+                ReturnDescription = "OK"
+            };
+        }
+
+        public bool SoftDeleteEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbResponse<User> UpdateEntity(User t, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
